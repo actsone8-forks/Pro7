138bae93e404ddad9c069128c5b6db75745b9c35
eelika lepasild <eelika.lepasild@mail.ee>
2022-01-03 18:45:46 +0000

    updates

--- backend/controllers/PostController.js
+++ backend/controllers/PostController.js
@@ -79,6 +79,66 @@ exports.createPost = (req, res, next) => {
       );
     };
 
+
+
+exports.likePost = async (req, res, next) => {
+  try {
+    const userId = token.getUserId(req);
+    const postId = req.params.id;
+    const user = await db.Like.findOne({
+      where: { UserId: userId, PostId: postId },
+    });
+    if (user) {
+      await db.Like.destroy(
+        { where: { UserId: userId, PostId: postId } },
+        { truncate: true, restartIdentity: true }
+      );
+      res.status(200).send({ messageReturn: "Post unlike successfully!!" });
+    } else {
+      await db.Like.create({
+        UserId: userId,
+        PostId: postId,
+      });
+      res.status(201).json({ messageReturn: "Post liked!" });
+    }
+  } catch (error) {
+    return res.status(500).send({ error: "Server error" });
+  }
+};
  
+exports.addComment = async (req, res) => {
+  try {
+    const comment = req.body.commentMessage;
+    const username = req.body.commentUsername;
+    const newComment = await db.Comment.create({
+      message: comment,
+      username: username,
+      UserId: token.getUserId(req),
+      PostId: req.params.id,
+    });
+
+    res
+      .status(201)
+      .json({ newComment, messageReturn: "Comment added successfully!!" });
+  } catch (error) {
+    return res.status(500).send({ error: "Server error" });
+  }
+};
+exports.deleteComment = async (req, res) => {
+  try {
+    const userId = token.getUserId(req);
+    const checkAdmin = await db.User.findOne({ where: { id: userId } });
+    const comment = await db.Comment.findOne({ where: { id: req.params.id } });
+
+    if (userId === comment.UserId || checkAdmin.admin === true) {
+      db.Comment.destroy({ where: { id: req.params.id } }, { truncate: true });
+      res.status(200).json({ message: "comment deleted successfully" });
+    } else {
+      res.status(400).json({ message: "Invalid operation" });
+    }
+  } catch (error) {
+    return res.status(500).send({ error: "Server error" });
+  }
+};
 
   
