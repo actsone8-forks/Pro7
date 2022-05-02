6aad5f2e279cc6ac6a2a232d2654e5848c6e07d5
eelika lepasild <eelika.lepasild@mail.ee>
2022-05-02 19:39:54 +0100

    working on posting a tweet

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="home">
     <div class="row">
-      <div class="col-md-3 homeColumn">Chit-chat at work!</div>
+      <div class="col-md-3 homeColumn"> Chit-chat at work!</div>
       <div class="col-md-9 homeColumn2">
-        <CreatePost />
+        <CreatePost :posts="posts" @postTweet="postTweet" />
+        <FeedPage :posts="posts" />
       </div>
     </div>
   </div>
@@ -11,18 +12,38 @@
 
 <script>
 import CreatePost from "../components/CreatePost.vue";
+import FeedPage from "../components/FeedPage.vue";
 
 export default {
   name: "HomeView",
+  data() {
+    return {
+      posts: [
+        {
+          user: "Tom",
+          post: "My first post here",
+        },
+        { user: "Eelika", post: "I`m feeling good today" },
+      ],
+    };
+  },
   components: {
     CreatePost,
+    FeedPage,
+  },
+  methods: {
+    postTweet(post) {
+      this.posts.push(post);
+    },
   },
 };
 </script>
 <style>
 .homeColumn {
-  background-color: aqua;
   height: 200px;
+  font-size: 30px;
+  color:#1c92d2;
+ 
 }
 .homeColumn2 {
   height: 200px;
