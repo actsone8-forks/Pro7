05d7e1ef4c16f54c6e6133269ecacd12aff9f525
eelika lepasild <eelika.lepasild@mail.ee>
2022-07-18 19:21:27 +0100

    change

--- frontend/src/components/UserCard.vue
+++ frontend/src/components/UserCard.vue
@@ -3,12 +3,12 @@
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
       <!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
       <path
-        d="M224 256c70.7 0 128-57.31 128-128s-57.3-128-128-128C153.3 0 96 57.31 96 128S153.3 256 224 256zM274.7 304H173.3C77.61 304 0 381.6 0 477.3c0 19.14 15.52 34.67 34.66 34.67h378.7C432.5 512 448 496.5 448 477.3C448 381.6 370.4 304 274.7 304z"
-      />
+        d="M224 256c70.7 0 128-57.31 128-128s-57.3-128-128-128C153.3 0 96 57.31 96 128S153.3 256 224 256zM274.7 304H173.3C77.61 304 0 381.6 0 477.3c0 19.14 15.52 34.67 34.66 34.67h378.7C432.5 512 448 496.5 448 477.3C448 381.6 370.4 304 274.7 304z" />
     </svg>
-    <div class="card-body" v-if="userInfo">
-      <h5 class="card-title" >{{ userInfo.username}}</h5>
-      <p class="card-text">{{userInfo.email}}</p>
+    <div class="card-body">
+      <h5 class="card-title">{{ userName }}</h5>
+      <h5 class="card-title">{{ email }}</h5>
+      <!-- <p class="card-text">{{ userInfo.email }}</p> -->
     </div>
     <ul class="list-group list-group-flush">
       <li class="list-group-item">
@@ -20,22 +20,26 @@
 <script>
 export default {
   computed: {
-   userInfo: () => {
-      return JSON.parse(localStorage.getItem("userInfo"))
-      ? JSON.parse(localStorage.getItem("userInfo"))
-      : {}
+    isLoggedIn() {
+      return this.$store.getters.isLoggedIn;
     },
-  },
-  mounted(){
-    if (!this.userInfo) {
-      location.reload()
+    isLoggedOut() {
+      return !this.isLoggedIn;
+    },
+    userName() {
+      return this.$store.getters.userName;
+      
+    },
+     email() {
+      return this.$store.getters.email;
     }
-  },
+  }
+
 };
 </script>
 
 <style>
-.card svg{
+.card svg {
   margin: 0 auto;
   width: 150px;
   height: 150px;
