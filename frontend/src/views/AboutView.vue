4e05c14dce17d76f08bb43d8a47959b44f1aa367
eelika lepasild <eelika.lepasild@mail.ee>
2022-09-10 18:59:46 +0100

    deleting in the parent folder

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -2,8 +2,9 @@
   <div class="about">
     <h3>Welcome to an internal social network for Groupomania's employees.</h3>
       <h4>The goal of this application is to facilitate more interaction between colleagues.</h4>
+      <h2>Let`s chat now!</h2>
   </div>
-  <h2>Let`s chat now!</h2>
+  
 </template>
 <script>
 export default {};
